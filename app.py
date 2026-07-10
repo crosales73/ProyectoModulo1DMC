57dc09081689ef939bed5a23d8831c3e6d009081
crosales73 <38187985+crosales73@users.noreply.github.com>
2026-07-09 22:10:07 -0500

    Add Streamlit title and sidebar parameters

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 import streamlit as st
+st.title("Proyecto módulo 1 Fundamentals")
+st.sidebar.title("Parámetros")
+
+valor_inicial = st.number_imput("ingrese el valor inicial", value = 0)
+valor_fimnal = st.number_imput("ingrese el valor final", value = 0)
