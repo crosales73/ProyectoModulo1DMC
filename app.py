b7dd36b95e2b562fc18b07291cf4796d77b03ad8
crosales73 <38187985+crosales73@users.noreply.github.com>
2026-07-09 22:16:43 -0500

    Fix typo in number_input function calls

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ st.sidebar.title("Parámetros")
 st.image("Python_logo.png")
 st.sidebar.image("DMC.png")
 
-valor_inicial = st.number_imput("ingrese el valor inicial", value = 0)
-valor_final = st.number_imput("ingrese el valor final", value = 1)
+valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
+valor_final = st.number_input("Ingrese el valor final", value = 1)
 
 lista_numerica = list(range(valor_inicial,valor_final))
 
