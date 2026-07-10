3683cf433f2e99cb043db0d9d92c2364ad7f3326
crosales73 <38187985+crosales73@users.noreply.github.com>
2026-07-09 22:14:16 -0500

    Fix typo in final value input and add images

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ import streamlit as st
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
+st.image("Python_logo.png")
+st.sidebar.image("DMC.png")
+
 valor_inicial = st.number_imput("ingrese el valor inicial", value = 0)
-valor_fimnal = st.number_imput("ingrese el valor final", value = 0)
+valor_final = st.number_imput("ingrese el valor final", value = 1)
+
+lista_numerica = list(range(valor_inicial,valor_final))
